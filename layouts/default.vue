3a44f8fbf9051c1450d97807b854bc22a400e10a
Liran Tal <liran.tal@gmail.com>
2021-09-11 18:11:56 +0300

    chore: initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app dark fixed>
+    <v-bottom-navigation>
+      <v-btn>
+        <span>GitHub Star</span>
+        <v-icon color="yellow">mdi-star</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Developer Advocate</span>
+        <v-icon color="green">mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Projects</span>
+        <v-icon>mdi-code-tags</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Security Activist</span>
+        <v-icon color="purple">mdi-security</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
+    <v-main>
+      <v-container mt-16>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Liran Tal',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-content>
+    <v-row>
+      <v-col class="text-center">
+        <v-avatar size="180">
+          <img src="https://github.com/lirantal.png" alt="John" />
+        </v-avatar>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col class="text-center"> Hello, I'm Liran Tal 👋 </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col class="text-center">
+        <v-btn icon link href="https://twitter.com/liran_tal" target="_blank">
+          <v-icon medium> mdi-twitter </v-icon>
+        </v-btn>
+        <v-btn icon link href="https://github.com/lirantal" target="_blank">
+          <v-icon medium> mdi-github </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col class="text-center">
+        <blockquote class="blockquote">
+          A GitHub Star, recognized for activisim in open source communities and
+          advancing web and Node.js security. Member of the Node.js Foundation
+          ecosystem security working group, Project lead and contributor to the
+          OWASP Foundation, and Developer Advocate at Snyk.
+        </blockquote>
+      </v-col>
+    </v-row>
+  </v-content>
+</template>
